a975fa02cd902eeccf4f1aa0d6b920fabf7f01d1
Bryan Woods <bryanwoods4e@gmail.com>
2016-11-03 13:01:39 -0400

    run everything twice

--- app.js
+++ app.js
@@ -22,3 +22,28 @@ blah = function(x, y) {
 blah()
 
 console.log("Testing hound...");
+
+const X = 1;
+const Y = 2;
+
+let xAndY = () => X + Y;
+
+let addTwo = (x, y) => {
+  return x + y;
+}
+
+console.log(xAndY());
+console.log(addTwo(1,2));
+
+x = "foo"
+y = "bar"
+
+console.log(x + y);
+
+blah = function(x, y) {
+  return "blah"
+}
+
+blah()
+
+console.log("Testing hound...");
