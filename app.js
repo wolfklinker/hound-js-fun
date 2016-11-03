9cd738ab60dba1a2fcbf7fa52ee348ac7c7b6a81
Bryan Woods <bryanwoods4e@gmail.com>
2016-11-03 12:51:21 -0400

    Adds some js

---
+++ app.js
@@ -0,0 +1,22 @@
+const X = 1;
+const Y = 2;
+
+let xAndY = () => X + Y;
+
+let addTwo = (x, y) => {
+  return x + y;
+}
+
+console.log(xAndY());
+console.log(addTwo(1,2));
+
+x = "foo"
+y = "bar"
+
+console.log(x + y);
+
+blah = function(x, y) {
+  return "blah"
+}
+
+blah()
